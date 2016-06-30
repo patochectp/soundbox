8b900a22ff78ec81f20d3c3370b4d8fe5156bfce
patoche <patrick.degeilh@canaltp.fr>
2016-06-30 15:15:44 +0200

    launch sound with keyboard

--- soundbox.py
+++ soundbox.py
@@ -1,5 +1,8 @@
 import threading
 import time
+import os
+import random
+import subprocess
 
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
@@ -40,16 +43,29 @@ class _GetchWindows:
 
 getch = _Getch()
 
+def play_sound(key):
+    sound_directory = '/home/pdegeilh/soundbox/sound/'
+    theme = 'theme_reunion_sncf'
+    map_key_sound= {
+        'l' : 'pipotte'
+    }
+    if map_key_sound.get(key):
+        path = os.path.join(sound_directory, theme, map_key_sound[key])
+        sound_files = os.listdir(path)
+        i = random.randrange(0, len(sound_files))
+        print('subprocess start')
+        subprocess.Popen(['mplayer', os.path.join(sound_directory, theme, map_key_sound[key], sound_files[i])], stdin=subprocess.PIPE).wait()
+        print('subprocess end')
+
 class KeyEventThread(threading.Thread):
     def run(self):
         keep_going = True
         while keep_going:
-            z = getch()
-            print (z)
-            if z == 'z':
+            key = getch()
+            if key == 'x':
                 keep_going = False
-
+            play_sound(key)
             time.sleep(0.1)
- 
+
 kethread = KeyEventThread()
 kethread.start()
