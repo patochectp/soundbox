54d63201d1ee034b10cfce518577bfc156124975
patoche <patrick.degeilh@canaltp.fr>
2016-07-01 11:22:25 +0200

    theme implementation

--- soundbox.py
+++ soundbox.py
@@ -6,24 +6,30 @@ import subprocess
 import config
 import datetime
 
+
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
+
     def __init__(self):
         try:
             self.impl = _GetchWindows()
         except ImportError:
             self.impl = _GetchUnix()
 
-    def __call__(self): return self.impl()
+    def __call__(self):
+        return self.impl()
 
 
 class _GetchUnix:
     def __init__(self):
-        import tty, sys
+        import tty
+        import sys
 
     def __call__(self):
-        import sys, tty, termios
+        import sys
+        import tty
+        import termios
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
         try:
@@ -44,35 +50,51 @@ class _GetchWindows:
 
 
 getch = _Getch()
-THEME = 'default'
 
-def play_sound(key):
+
+def play_sound(key, theme_id):
     category = config.KEY_CATEGORY_MAPPING.get(key)
+    theme = config.THEME.get(theme_id % len(config.THEME))
     print(category)
     if category:
-        path = os.path.join(config.SOUND_DIRECTORY, THEME, category)
+        path = os.path.join(config.SOUND_DIRECTORY, theme, category)
+        print(path)
+        if not os.path.exists(path):
+            return
         sound_files = os.listdir(path)
         if len(sound_files) == 0:
             return
         i = random.randrange(0, len(sound_files))
         print('subprocess start')
-        subprocess.Popen([config.PLAYER, os.path.join(config.SOUND_DIRECTORY, THEME, category, sound_files[i])], stdin=subprocess.PIPE).wait()
+        subprocess.Popen([config.PLAYER, os.path.join(config.SOUND_DIRECTORY, theme, category, sound_files[i])],
+                         stdin=subprocess.PIPE).wait()
         print('subprocess end')
+
 
 class KeyEventThread(threading.Thread):
     def run(self):
+        theme_id = 0
         while True:
-            key = ord(getch())
+            key = ord(next(inp for inp in getch()))
+            # key = ord(getch())
             print(key)
             if key == 120:
                 break
+            if key == 27:
+                theme_id += 1
             if config.KEY_CATEGORY_MAPPING.get(key) is None:
-                continue 
+                continue
+            if config.THEME.get(theme_id % len(config.THEME)) is None:
+                continue
             with open("stat.csv", "a") as myfile:
-                myfile.write("{datetime}, {theme}, {category}\n".format(datetime=datetime.datetime.now(), theme=THEME, category=config.KEY_CATEGORY_MAPPING.get(key)))
+                myfile.write("{datetime}, {theme}, {category}\n".format(datetime=datetime.datetime.now(),
+                                                                        theme=config.THEME.get(
+                                                                            theme_id % len(config.THEME)),
+                                                                        category=config.KEY_CATEGORY_MAPPING.get(key)))
 
-            play_sound(key)
+            play_sound(key, theme_id)
             time.sleep(0.1)
+
 
 kethread = KeyEventThread()
 kethread.start()
